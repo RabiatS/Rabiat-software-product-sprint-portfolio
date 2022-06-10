d103be39f147473fb6fad4076410f370c0f4f335
rabiat <robbiahsadiq@gmail.com>
2022-06-10 09:25:57 +0000

    Added video to top page and edits, more walk through followed

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,15 +15,8 @@
 /**
  * Adds a random greeting to the page.
  */
+ 
 
-var viewportHeader = document.querySelector(".viewport-header");
-
-document.body.addEventListener("scroll", function(event) {
-  var opacity = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
-  var scale = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
-  document.documentElement.style.setProperty('--headerOpacity', opacity);
-  document.documentElement.style.setProperty('--headerScale', scale);
-});
 
 function addRandomGreeting() {
   const greetings =
